acee2df12e9a7e788a9549a436910ae5fdbf00ad
Hien Nguyen <htn9zzb@virginia.edu>
2021-03-14 12:51:24 -0400

    logout feature

--- projectB27/urls.py
+++ projectB27/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="exercisegamification/index.html")),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('logout', LogoutView.as_view()),
+    path('logout', LogoutView.as_view(), name='logout'),
 ]
