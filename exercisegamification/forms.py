4e48e3e9898b83a95f9d412ca17e10150db25496
Hien Nguyen <htn9zzb@virginia.edu>
2021-04-06 19:56:36 -0400

    pfp w/o goals frontend styling

--- exercisegamification/forms.py
+++ exercisegamification/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.forms import ModelForm, Field
-from .models import Profile
+from .models import Profile, Goal
 
 class EditProfileForm(ModelForm):
     first_name = forms.CharField(max_length=50, required=True)
@@ -10,6 +10,8 @@ class EditProfileForm(ModelForm):
     age = forms.IntegerField(max_value=None,min_value=0, label="Age")
     weight = forms.IntegerField(max_value=None, min_value=0, required=False, label='Weight*')
     bmi = forms.IntegerField(max_value=100, min_value=0, required=False, label='BMI*')
+    fav_exercise = forms.CharField(max_length=500, required=True)
+    profile_pic = forms.ImageField(required=False, label='Profile Picture')
 
     class Meta:
         model = Profile
@@ -18,5 +20,55 @@ class EditProfileForm(ModelForm):
             'last_name',
             'age',
             'weight',
-            'bmi'
+            'bmi',
+            'fav_exercise',
+            'profile_pic'
         )
+
+class AddGoalForm(ModelForm):
+    title = forms.CharField(max_length=50, required=True)
+    pub_date = forms.DateTimeField(required=True, label="Date Created")
+    reach_date = forms.DateTimeField(required=True, label='Target Reach Date')
+    goal_text = forms.CharField(required=True)
+    accomplished = forms.BooleanField(required=True)
+
+    class Meta:
+        model = Goal
+        fields = (
+            'title',
+            'pub_date',
+            'reach_date',
+            'goal_text',
+            'accomplished'
+        )
+
+class EditGoalForm(ModelForm):
+    title = forms.CharField(max_length=50, required=True)
+    pub_date = forms.DateTimeField(required=True, label="Date Created")
+    reach_date = forms.DateTimeField(required=True, label='Target Reach Date')
+    goal_text = forms.CharField(required=True)
+    accomplished = forms.BooleanField(required=True)
+
+    class Meta:
+        model = Goal
+        fields = (
+            'title',
+            'pub_date',
+            'reach_date',
+            'goal_text',
+            'accomplished'
+        )
+
+#class AddGoalForm(forms.ModelForm):
+#    class Meta:
+#        model = Goal
+#        fields = ('author', 'title', 'pub_date', 'reach_date', 'goal_text', 'accomplished')
+
+#        widgets = {
+#            'author': forms.Select(attrs={'class':'form-control'}),
+#            'title': forms.TextInput(attrs={'class':'form-control'}),
+#            'pub_date': forms.SelectDateWidget(attrs={'class':'form-control'}),
+#            'reach_date': forms.SelectDateWidget(attrs={'class':'form-control'}),
+#            'goal_text': forms.Textarea(attrs={'class':'form-control'}),
+#            'accomplished': forms.BooleanField(),
+#        }
--- projectB27/urls.py
+++ projectB27/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LogoutView
 from exercisegamification import views
@@ -31,5 +33,6 @@ urlpatterns = [
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('goals/', views.GoalsView.as_view(), name='goals list'),
     path('goals/<int:pk>', views.GoalDetailView, name='goal detail'),
+    path('goals/create/', views.AddGoalView, name='add goal'),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
