1352c8f08f6fd87dfac2d0a29a5a785ca1920e93
pidnam1 <pidnam1@gmail.com>
2021-05-05 18:54:51 -0400

    testing db set up

--- exercisegamification/tests.py
+++ exercisegamification/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from exercisegamification.models import PointAchievement, Profile
+
 
 # Create your tests here.
+
+class PointAchievementNameTest(TestCase):
+
+    def setUp(self):
+        PointAchievement.objects.create(author = None, achievement_title="Go Getter", achievement_text = "Run", achievement_threshold = 200)
+
+    def test_get_title(self):
+        go_getter = PointAchievement.objects.get(achievement_title="Go Getter")
+        self.assertEqual(go_getter.achievement_title, str(go_getter))
+
+class PointAchievementForeignKey(TestCase):
+
+    def setUp(self):
+
+        PointAchievement.objects.create(author = None, achievement_title="Go Getter", achievement_text = "Run", achievement_threshold = 200)
+
+    def test_get_title(self):
+        go_getter = PointAchievement.objects.get(achievement_title="Go Getter")
+        self.assertEqual(go_getter.achievement_title, str(go_getter))
--- projectB27/settings.py
+++ projectB27/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import django_heroku
 import os
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -85,6 +86,8 @@ WSGI_APPLICATION = 'projectB27.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -94,7 +97,15 @@ DATABASES = {
         'USER' : 'qxefyjrpmaysbw',
         'PASSWORD' : '4501f9927ac207fe365ab4f5ddde595f4d5e13f3db325e2b6a225854e056a12c',
     },
+
+
 }
+
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
+    }
 
 
 # Password validation
